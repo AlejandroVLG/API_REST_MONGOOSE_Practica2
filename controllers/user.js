42d519674a3f4ba571ef7c77e33f25cfe7985c7c
Alexlg <Alejandrolg86@gmail.com>
2022-11-01 17:31:19 +0100

    refactoring auth/middleware and index/service

--- controllers/user.js
+++ controllers/user.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const mongoose = require('mongoose')
+const user = require('../models/user')
 
 const User = require('../models/user')
 const service = require('../services')
@@ -9,6 +10,7 @@ function signUp(req, res) {
     const user = new User({
         email: req.body.email,
         displayName: req.body.displayName,
+        password: req.body.password
     })
 
     user.save((err) => {
@@ -21,10 +23,19 @@ function signUp(req, res) {
 }
 
 function signIn(req, res) {
+    user.find({ email: req.body.email }, (err, user) => {
+        if (err) return res.status(500).send({ message: err })
+        if (!user) return res.status(404).send({ message: 'No existe el usuario' })
 
+        req.user = user
+        res.status(200).send({
+            message: 'Te has logeado correctamente',
+            token: service.createToken(user)
+        })
+    })
 }
 
 module.exports = {
-    signIn,
-    signUp
+    signUp,
+    signIn
 }
--- routes/index.js
+++ routes/index.js
@@ -11,15 +11,15 @@ api.post('/product', productCtrl.createProduct)
 api.put('/product/:productId', productCtrl.updateProducts)
 api.delete('/product/:productId', productCtrl.deleteProduct)
 
-api.get('/private', auth.isAuth, function (req, res) {
+api.get('/private', auth, function (req, res) {
     res.status(200).send({ message: 'Tienes acceso' })
 })
 
-api.get('/users', userCtrl)
+/* api.get('/users', userCtrl)
 api.get('/user/:userId', userCtrl)
 api.post('/user', userCtrl)
 api.put('/user/:userId', userCtrl)
-api.delete('/user/:userId', userCtrl)
+api.delete('/user/:userId', userCtrl) */
 
 
 module.exports = api 
